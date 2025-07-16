00407999a32d375103d5ffed975366c79f9ba3a4
AhmedEssamYassin <eng.ahmedessamyassin@gmail.com>
2025-07-17 02:26:21 +0300

    feat: implement Miller-Rabin (Jim Sinclair)

---
+++ Topic-based Training/Number Theory/PrimeChecking_Miller_Rabin_Jim Sinclair.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define endl "\n"
+
+template <typename T>
+T mult64(const T &a, const T &b, T mod)
+{
+    return (__int128_t)a * b % mod;
+}
+
+template <typename T>
+T modPow(T N, T power, T mod)
+{
+    if (N % mod == 0 || N == 0)
+        return 0;
+    if (N == 1 || power == 0)
+        return 1;
+    T res{1};
+    while (power)
+    {
+        if (power & 1)
+            res = mult64(res, N, mod);
+        N = mult64(N, N, mod);
+        power >>= 1;
+    }
+    return res;
+}
+
+template <typename T>
+bool isPrime(T N)
+{
+    if (N < 2 || N % 6 % 4 != 1)
+        return (N | 1) == 3;
+    T d = N - 1;
+    int s{};
+    while (~s & 1)
+        d >>= 1, ++s;
+    for (const T &a : {2, 3, 5, 7, 11, 13, 325, 9375, 28178, 450775, 9780504, 1795265022})
+    {
+        T p = modPow(a % N, d, N), i = s;
+        while (p != 1 && p != N - 1 && a % N && i--)
+            p = mult64(p, p, N);
+        if (p != N - 1 && i != s)
+            return false;
+    }
+    return true;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+#endif //! ONLINE_JUDGE
+    int t = 1;
+    ll N;
+    cin >> t;
+    while (t--)
+    {
+        cin >> N;
+        if (isPrime(N))
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    return 0;
+}
