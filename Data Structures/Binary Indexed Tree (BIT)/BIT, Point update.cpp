eb82539140f197ce0566a194d1ef1757e5fb66f4
AhmedEssamYassin <eng.ahmedessamyassin@gmail.com>
2025-08-21 14:55:33 +0300

    feat: add Fenwick Tree class

--- Data Structures/Binary Indexed Tree (BIT)/BIT, Point update.cpp
+++ Data Structures/Binary Indexed Tree (BIT)/BIT, Point update.cpp
@@ -7,33 +7,43 @@ using namespace std;
 
 /*
 Fenwick Tree: Binary Indexed Tree (BIT)
-1-based (because (0 & -0) would make an infinite loop)
+0-based version
 Works for invertible operations (such as Summation (+) and XOR (^))
 */
-const int N = 200010;
-int BIT[N + 1];
-
-void Update(int pos, const int &delta)
+class FenwickTree
 {
-    for (int i = pos; i <= N; i += (i & -i))
-        BIT[i] += delta;
-}
+private:
+    int N;
+    vector<ll> BIT;
 
-ll query(int pos)
-{
-    ll sum = 0;
-    for (int i = pos; i; i -= (i & -i))
-        sum += BIT[i];
+public:
+    FenwickTree(int sz)
+    {
+        N = sz;
+        BIT.assign(N + 1, 0);
+    }
+    void update(int pos, const int &delta)
+    {
+        for (int i = pos; i < N; i |= (i + 1))
+            BIT[i] += delta;
+    }
 
-    return sum;
-}
+    ll query(int pos)
+    {
+        ll sum = 0;
+        for (int i = pos; i >= 0; i = (i & (i + 1)) - 1)
+            sum += BIT[i];
 
-ll rangeQuery(int L, int R)
-{
-    if (L > R || L <= 0)
-        return 0; // Neutral value
-    return (query(R) - query(L - 1));
-}
+        return sum;
+    }
+
+    ll rangeQuery(int L, int R)
+    {
+        if (L > R || L <= 0)
+            return 0; // Neutral value
+        return (query(R) - query(L - 1));
+    }
+};
 
 int main()
 {
@@ -49,17 +59,18 @@ int main()
     {
         int N;
         cin >> N;
-        int *arr = new int[N + 1];
+        vector<ll> arr(N + 1);
+        FenwickTree fenTree(N);
         // Inputting and initializing
         for (int i = 1; i <= N; i++)
-            cin >> arr[i], BIT[i] = 0;
+            cin >> arr[i];
 
         int res = 0;
         ll ans = 0;
         for (int i = 1; i <= N; i++)
         {
-            ans += i - 1 - query(arr[i] - 1);
-            Update(arr[i], 1);
+            ans += i - 1 - fenTree.query(arr[i] - 1);
+            fenTree.update(arr[i], 1);
         }
         cout << ans << endl;
     }
---
+++ Data Structures/Binary Indexed Tree (BIT)/Game of Coders 4/Solution.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define endl "\n"
+
+/*
+Fenwick Tree: Binary Indexed Tree (BIT)
+0-based version
+Works for invertible operations (such as Summation (+) and XOR (^))
+*/
+class FenwickTree
+{
+private:
+	int N;
+	vector<ll> BIT;
+
+public:
+	FenwickTree(int sz)
+	{
+		N = sz;
+		BIT.assign(N + 1, 0);
+	}
+	void update(int pos, const int &delta)
+	{
+		for (int i = pos; i < N; i |= (i + 1))
+			BIT[i] += delta;
+	}
+
+	ll query(int pos)
+	{
+		ll sum = 0;
+		for (int i = pos; i >= 0; i = (i & (i + 1)) - 1)
+			sum += BIT[i];
+
+		return sum;
+	}
+
+	ll rangeQuery(int L, int R)
+	{
+		if (L > R || L <= 0)
+			return 0; // Neutral value
+		return (query(R) - query(L - 1));
+	}
+};
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("Output.txt", "w", stdout);
+#endif //! ONLINE_JUDGE
+	int t = 1;
+	// cin >> t;
+	while (t--)
+	{
+		int N, Q;
+		cin >> N >> Q;
+		vector<ll> A(N);
+		vector<tuple<int, int, int, int, int, int>> queries; // {R, sign, L, x, y, idx}
+
+		for (int i{}; i < N; i++)
+			cin >> A[i];
+
+		for (int i = 0; i < Q; i++)
+		{
+			int L, R, X, Y;
+			cin >> L >> R >> X >> Y;
+			queries.emplace_back(R, +1, L, X, Y, i);
+			queries.emplace_back(L - 1, -1, L, X, Y, i);
+		}
+
+		sort(queries.begin(), queries.end());
+		vector<ll> result(Q);
+		int curr = 0;
+		FenwickTree fenTree(2e5);
+		for (auto &[r, sign, l, x, y, idx] : queries)
+		{
+			while (curr < N && curr < r)
+			{
+				fenTree.update(A[curr], 1);
+				++curr;
+			}
+			ll count = fenTree.rangeQuery(x, y);
+			result[idx] += sign * count;
+		}
+
+		for (const ll &res : result)
+			cout << res << endl;
+	}
+	return 0;
+}
